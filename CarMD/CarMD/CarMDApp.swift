359435d9f653a0b08359eb4d005c3ce413414c9b
Angela Nguyen <angelang@usc.edu>
2024-03-27 16:44:24 -0700

    create ios project

---
+++ CarMD/CarMD/CarMDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarMDApp.swift
+//  CarMD
+//
+//  Created by Angela Nguyen on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct CarMDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
